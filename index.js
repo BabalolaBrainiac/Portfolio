c0f01d12db5cbfbcbc0f96eb0d32b3ce999d4d7c
BabalolaBrainiac <74297323+BabalolaBrainiac@users.noreply.github.com>
2021-07-12 14:30:14 +0100

    New  Github Pages Portfolio
    Index HTML, CS, and JS file

---
+++ index.js
@@ -0,0 +1,16 @@
+const Nav = document.querySelector(".main-navigation");
+const overlay = document.querySelector(".overlay");
+const toggle = document.querySelector(".navbar-toggler");
+
+const openNav = () => {
+  Nav.classList.add("active");
+};
+const closeNav = () => {
+  Nav.classList.remove("active");
+};
+
+toggle.addEventListener("click", openNav);
+overlay.addEventListener("click", closeNav);
+
+document.addEventListener("swiped-right", openNav);
+document.addEventListener("swiped-left", closeNav);
